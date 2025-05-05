c2add6a339bfaf1a562cbadc7fa7025e04c5856a
ahmadsayud <ahmedabualkhaur@gmail.com>
2025-05-05 13:54:14 +0300

    Updated Calculator component and reducer

--- src/components/calculatorReducer.js
+++ src/components/calculatorReducer.js
@@ -1,20 +1,3 @@
-// export function calculatorReducer(state, action) {
-//   const { firstNumberInput, secondNumberInput } = action.payload;
-
-//   switch (action.type) {
-//     case "SUM":
-//       return Number(firstNumberInput) + Number(secondNumberInput);
-//     case "SUBTRACT":
-//       return Number(firstNumberInput) - Number(secondNumberInput);
-//     case "MULTIPLY":
-//       return Number(firstNumberInput) * Number(secondNumberInput);
-//     case "DIVIDE":
-//       return Number(firstNumberInput) / Number(secondNumberInput);
-//     default:
-//       return state;
-//   }
-// }
-
 export function calculatorReducer(state, { type, payload }) {
   const { firstNumberInput, secondNumberInput } = payload;
 
